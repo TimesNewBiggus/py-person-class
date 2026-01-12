d5fde353f47e464347265fbcea168900b12e5baa
Dmytro Ponomarenko <dmytro.g.ponomarenko@gmail.com>
2026-01-12 13:35:18 +0200

    Solution redacted 1

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ class Person:
 
 
 def create_person_list(people: list[dict]) -> list[Person]:
+    Person.people.clear()
     created_list = [Person(person.get("name"),
                            person.get("age"))
                     for person in people]
